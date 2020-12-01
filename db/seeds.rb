0a46ec43ca27fd93a8ba43e61e9fe36e703c22ad
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-01 23:28:19 +0100

    FEAT: Added test data to play with

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Clearing Users"
+User.destroy_all
+
+User.create(email: "sy@mangotree.dev", password: "password", admin: true)
+User.create(email: "sheila@mangotree.dev", password: "password", admin: false)
+User.create(email: "nico@mangotree.dev", password: "password", admin: false)
+
+puts "Created #{User.count} users 🎉"
